4086161a90fb47f0d5e1a6c0eb3e1a407b9157b5
Dev K <tranvantu.dev@gmail.com>
2019-12-18 11:59:37 +0700

    init

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create!(name: "Tan Thi Ha",
-             email: "tanthiha201198@gmail.com",
+             email: "tanthiha@gmail.com",
              password: "123456",
              password_confirmation: "123456",
              phone: "0764423228",
